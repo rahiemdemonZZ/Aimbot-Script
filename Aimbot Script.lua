451a775915c4875751991d9d5ccba9c9b4654401
rahiemdemonZZ <mortimer766@gmail.com>
2024-09-02 14:19:50 -0400

    Update Aimbot Script.lua

--- Aimbot Script.lua
+++ Aimbot Script.lua
@@ -9,15 +9,15 @@ local Holding = false
 _G.AimbotEnabled = true
 _G.TeamCheck = false -- If set to true then the script would only lock your aim at enemy team members.
 _G.AimPart = "Head" -- Where the aimbot script would lock at.
-_G.Sensitivity = 0 -- How many seconds it takes for the aimbot script to officially lock onto the target's aimpart.
+_G.Sensitivity = 20 -- How many seconds it takes for the aimbot script to officially lock onto the target's aimpart.
 
 _G.CircleSides = 64 -- How many sides the FOV circle would have.
 _G.CircleColor = Color3.fromRGB(255, 255, 255) -- (RGB) Color that the FOV circle would appear as.
 _G.CircleTransparency = 0.7 -- Transparency of the circle.
-_G.CircleRadius = 80 -- The radius of the circle / FOV.
-_G.CircleFilled = false -- Determines whether or not the circle is filled.
-_G.CircleVisible = true -- Determines whether or not the circle is visible.
-_G.CircleThickness = 0 -- The thickness of the circle.
+_G.CircleRadius = 44-- The radius of the circle / FOV.
+_G.CircleFilled = True -- Determines whether or not the circle is filled.
+_G.CircleVisible = False -- Determines whether or not the circle is visible.
+_G.CircleThickness = 1 -- The thickness of the circle.
 
 local FOVCircle = Drawing.new("Circle")
 FOVCircle.Position = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
@@ -94,7 +94,7 @@ RunService.RenderStepped:Connect(function()
     FOVCircle.NumSides = _G.CircleSides
     FOVCircle.Thickness = _G.CircleThickness
 
-    if Holding == true and _G.AimbotEnabled == true then
+    if Holding == true and _G.AimbotEnabled == true then:'78,
         TweenService:Create(Camera, TweenInfo.new(_G.Sensitivity, Enum.EasingStyle.Sine, Enum.EasingDirection.Out), {CFrame = CFrame.new(Camera.CFrame.Position, GetClosestPlayer().Character[_G.AimPart].Position)}):Play()
     end
 end)
